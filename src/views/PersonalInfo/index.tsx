a6b94356434a960b97a92ce7912f8c79ce8c6748
czx <70849018+HAjiMa666@users.noreply.github.com>
2023-03-20 19:40:41 +0800

    feat: 音乐项目添加扫码登录功能

--- src/views/PersonalInfo/index.tsx
+++ src/views/PersonalInfo/index.tsx
@@ -1,14 +1,38 @@
-import React, { memo, useState } from 'react'
+import React, { memo, useState, useEffect } from 'react'
 import type { FC, ReactNode } from 'react'
-import { Button, Modal, Form, Input } from 'antd'
+import { Button, Modal, Form, Input, Tabs, message } from 'antd'
 import { LoginModalWrapper, PersonInfoWrapper } from './style'
-
+import {
+  getQRCodeKey,
+  getQRCodeImg,
+  checkIsScanQRCode
+} from './services/personInfo'
+import { QRCodeSVG } from 'qrcode.react'
 interface IProps {
   children?: ReactNode
 }
 
 const PersonalInfo: FC<IProps> = () => {
   const [loginModal, setLoginModal] = useState(false)
+  const [qrKey, setQRKey] = useState('')
+  const [qrUrl, setQRUrl] = useState('')
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      checkIsScanQRCode(qrKey).then((res) => {
+        if (res.code === 803) {
+          document.cookie = res.cookie
+          clearInterval(timer)
+          message.success('登录成功')
+        }
+      })
+    }, 3000)
+    return () => {
+      console.log('清除')
+      clearInterval(timer)
+    }
+  }, [qrUrl])
+
   return (
     <PersonInfoWrapper className="personInfoWrapper">
       <Button
@@ -29,47 +53,78 @@ const PersonalInfo: FC<IProps> = () => {
             setLoginModal(false)
           }}
           closable={false}
-          okText="登录"
-          cancelText="取消"
           className="loginModal"
+          footer={null}
           getContainer={() =>
             document.querySelector('.loginModalWrapper') as HTMLDivElement
           }
         >
-          <Form
-            labelCol={{ span: 3 }}
-            labelAlign="left"
-            wrapperCol={{
-              span: 20,
-              offset: 1
+          <Tabs
+            onTabClick={(activeKey) => {
+              if (activeKey === '2') {
+                getQRCodeKey().then((res) => {
+                  const key = res.data.unikey
+                  setQRKey(key)
+                  getQRCodeImg(key).then((res) => {
+                    console.log(res.data)
+                    const qrUrl = res.data.qrurl
+                    setQRUrl(qrUrl)
+                  })
+                })
+              }
             }}
-            name="basic"
-            initialValues={{ remember: true }}
-            // onFinish={onFinish}
-            // onFinishFailed={onFinishFailed}
-          >
-            <Form.Item
-              label="用户名"
-              name="username"
-              rules={[{ required: true, message: '请输入你的用户名!' }]}
-            >
-              <Input />
-            </Form.Item>
+            items={[
+              {
+                label: '账号密码登录',
+                key: '1',
+                children: (
+                  <Form
+                    labelCol={{ span: 3 }}
+                    labelAlign="left"
+                    wrapperCol={{
+                      span: 20,
+                      offset: 1
+                    }}
+                    name="basic"
+                    initialValues={{ remember: true }}
+                    // onFinish={onFinish}
+                    // onFinishFailed={onFinishFailed}
+                  >
+                    <Form.Item
+                      label="用户名"
+                      name="username"
+                      rules={[{ required: true, message: '请输入你的用户名!' }]}
+                    >
+                      <Input />
+                    </Form.Item>
 
-            <Form.Item
-              label="密码"
-              name="password"
-              rules={[{ required: true, message: '请输入你的密码!' }]}
-            >
-              <Input.Password />
-            </Form.Item>
+                    <Form.Item
+                      label="密码"
+                      name="password"
+                      rules={[{ required: true, message: '请输入你的密码!' }]}
+                    >
+                      <Input.Password />
+                    </Form.Item>
 
-            <Form.Item wrapperCol={{ span: 24 }}>
-              <Button type="primary" htmlType="submit" block>
-                登录
-              </Button>
-            </Form.Item>
-          </Form>
+                    <Form.Item wrapperCol={{ span: 24 }}>
+                      <Button type="primary" htmlType="submit" block>
+                        登录
+                      </Button>
+                    </Form.Item>
+                  </Form>
+                )
+              },
+              {
+                label: '扫码登录',
+                key: '2',
+                children: (
+                  <div style={{ textAlign: 'center' }}>
+                    <QRCodeSVG value={qrUrl} level="H" size={200} />
+                  </div>
+                )
+              }
+            ]}
+          ></Tabs>
         </Modal>
       </LoginModalWrapper>
     </PersonInfoWrapper>
--- src/views/PersonalInfo/services/personInfo.ts
+++ src/views/PersonalInfo/services/personInfo.ts
@@ -1,7 +1,19 @@
 import { zxRequest } from '@/services'
 
-export function QRCodeLogin() {
+export function getQRCodeKey() {
   return zxRequest.get({
-    url: '/login/qr/key'
+    url: `/login/qr/key?timeStamp=${new Date().getTime()}`
+  })
+}
+
+export function getQRCodeImg(key: string | number, qrimg?: boolean) {
+  return zxRequest.get({
+    url: `/login/qr/create?timeStamp=${new Date().getTime()}&&key=${key}&&qrimg=${qrimg}`
+  })
+}
+
+export function checkIsScanQRCode(key: string | number) {
+  return zxRequest.get({
+    url: `/login/qr/check?timeStamp=${new Date().getTime()}&&key=${key}`
   })
 }
